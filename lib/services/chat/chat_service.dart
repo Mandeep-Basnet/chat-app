5e974e9a3e69d7bb34d369c6e4a7ae5c610db386
Mandeep-Basnet <np05cp4a230108@iic.edu.np>
2024-08-13 11:19:28 +0545

    4th Commit

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -17,6 +17,26 @@ class ChatService {
   }
 
   //Method to get the receiver's ID based on their email
+  Future<String?> getReceiverID(String email) async {
+    try {
+      //Query the firestore for user with the specified email
+      final snapshot = await _firestore
+          .collection('Users')
+          .where('email', isEqualTo: email)
+          .get();
+      //Return the receiver's ID
+      if (snapshot.docs.isNotEmpty) {
+        final user = snapshot.docs.first.data() as Map<String, dynamic>;
+      } else {
+        print('Receiver not found');
+        return null;
+      }
+    } catch (e) {
+      //Printing the error
+      print('error fetching receiver ID : $e');
+      return null;
+    }
+  }
 
   //Method to send message to the specified receiver
 
